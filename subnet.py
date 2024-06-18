e4b839bc442a95ac00aa736a98886be20713a418
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-06-18 15:16:58 +0800

    Added Variables

---
+++ subnet.py
@@ -0,0 +1,12 @@
+# Credential and IP for making API Calls
+IP_PC = "@@{ip_pc}@@"
+USER_PC = "@@{pc_cred.username}@@"
+PASS_PC = "@@{pc_cred.secret}@@"
+
+# Network variables for creating subnet
+UUID_VPC = "@@{vpc_uuid}@@"
+OVERLAY_NAME = "@@{subnet_name}@@"
+OVERLAY_PREFIX_LEN = "@@{prefix_length}@@"
+OVERLAY_IP = "@@{ip_subnet}@@"
+OVERLAY_GATEWAY = "@@{ip_gateway}@@"
+OVERLAY_POOL = "@@{ip_pool_range}@@"
