3ffd7253e6bac6e8867f61804ae89294b5fd7dbd
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-07-05 14:36:04 +0800

    Initial Commit

---
+++ vpc.py
@@ -0,0 +1,10 @@
+import requests
+
+IP_PC = "@@{ip_pc}@@"
+USER_PC = "@@{pc_cred.username}@@"
+PASS_PC = "@@{pc_cred.secret}@@"
+
+NAME_VPC = "@@{vpc_name}@@"
+UUID_UNDERLAY_SUBNET = "@@{underlay_uuid}@@"
+NAME_UNDERLAY_SUBNET = "@@{underlay_name}@@"
+
